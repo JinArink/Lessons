f5349471b778d5a21a8b684ef86c66d8a8bb7d67
JinArink <jin.arink@gmail.com>
2014-04-11 14:56:48 +0200

    circle-line

--- Panda/src/game/main.js
+++ Panda/src/game/main.js
@@ -6,31 +6,19 @@ game.module(
 )
 .body(function() {
 
-game.addAsset('logo.png', 'logo');
+//game.addAsset('logo.png', 'logo');
 
 SceneGame = game.Scene.extend({
     backgroundColor: 0x808080,
 
     init: function() {
-        var logo = new game.Sprite('logo');
-        logo.anchor.set(0.5, 0.5);
-        logo.position.set(game.system.width / 2, game.system.height / 2);
-        this.stage.addChild(logo);
+        var shape = new game.Circle(100);
+        shape.anchor.set(0.5, 0.5);
+        shape.position.set(game.system.width / 2, game.system.height / 2);
+        this.stage.addChild(shape);
         
-        var tween = new game.Tween(logo.position);
+        var tween = new game.Tween(shape.position);
         tween.to({x: game.system.width / 2 - 10}, 500);
-        tween.repeat();
-        tween.yoyo();
-        tween.start();
-        
-        var logo2 = new game.Sprite('logo');
-        logo2.anchor.set(0.5, 0.5);
-        logo2.alpha = 0.5;
-        logo2.position.set(game.system.width / 2, game.system.height / 2);
-        this.stage.addChild(logo2);
-        
-        var tween = new game.Tween(logo2.position);
-        tween.to({y: game.system.width / 2 - 10}, 500);
         tween.repeat();
         tween.yoyo();
         tween.start();
